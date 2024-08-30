3a322d0dfcaf71012ff31065d506311b54ebbcf8
Benjamin Johnson <bjohnson@fcusd.org>
2024-08-30 09:54:04 -0700

    Update Program3.java

--- Program3.java
+++ Program3.java
@@ -4,7 +4,36 @@ import java.util.Scanner; //This imports the console scanner that reads user inp
 
 //Your code here:
 
+    //Variables
+    int length = 0;
+    int width = 0;
+    int area = 0;
+    int perimeter = 0;
+    
+    //Ask for user input
+    System.out.println("Enter Length:");
+    //Get Input
+    length = myScanner.nextInt(); 
+    
+    System.out.println("Enter Width:");
+    width = myScanner.nextInt();
+    
+    System.out.println("Enter Area:");
+    //Get Input
+    area = myScanner.nextInt();
+    
+    System.out.println("Enter Perimeter:");
+    //Get Input
+    perimeter = myScanner.nextInt();
+    
+    //Output
+    System.out.println ("Length is:" + length);
+    System.out.println ("width is:" + width);
+    System.out.println ("area is:" + area);
+    System.out.println ("perimeter is:" + perimeter);
+     System.out.println ("This program was written by Mahita Gadiraju on 8/13/24");
 
+}
 
 
 //Paste console output below:
